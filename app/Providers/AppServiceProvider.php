f8e3f7a897efadae1d9ce4fcded06e5d05abbe3a
souravtah <souravtah@gmail.com>
2022-04-03 23:57:15 +0530

    AppServiceProvider modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+//use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        //Paginator::useBootstrapFive();
+        if(env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
